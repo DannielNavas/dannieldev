1cf7a4caeec33c589f625ade2b0a607bc14b5b2d
DannielDev <dannielnavas@gmail.com>
2022-01-10 16:57:54 -0500

    💄 New style page

--- src/app/shared/components/aboutmecomponents/stack/stack.component.spec.ts
+++ src/app/shared/components/aboutmecomponents/stack/stack.component.spec.ts
@@ -1,3 +1,4 @@
+import { SimpleChanges } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StackComponent } from './stack.component';
@@ -8,9 +9,8 @@ describe('StackComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StackComponent ]
-    })
-    .compileComponents();
+      declarations: [StackComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +21,52 @@ describe('StackComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should ngOnchange playlist', () => {
+    const fakeSimpleChanges = {} as SimpleChanges;
+    component.ngOnChanges(fakeSimpleChanges);
+  });
+  it('should ngOnchange principalData playlist', () => {
+    const fakeSimpleChanges = {} as SimpleChanges;
+    component.stack = {
+      software: {
+        items: [
+          'IDE: VSCode',
+          'Tema: Moonlight II Italic',
+          'Terminal: Git bash (Windows) Iterm2 (MACOS)',
+          'Fuente: Victor mono (Como instalar)',
+          'Navegador: Microsoft Edge',
+          'Escritura y organización: Notion',
+        ],
+        title: 'Software',
+      },
+      hardware: {
+        title: 'Hardware:',
+        items: {
+          perifericos: {
+            Auriculares: '',
+            Micrófono: '',
+            Monitor: '',
+            Teclado: 'VSG Bellatrix',
+          },
+          laptop: 'Laptop:',
+          desktop: {
+            items: [
+              'CPU: AMD Rayzen 7 3700X',
+              'Placa base: Asus rog stick ',
+              'Memoria: 32 GB',
+              'Almacenamiento: 500 GB M2',
+              'Tarjeta grafica: asus 710',
+              'Fuente de alimentación:',
+              'Gabinete: XPG',
+            ],
+            title: 'Pc escritorio:',
+          },
+        },
+      },
+      title: 'Mi stack',
+    };
+
+    component.ngOnChanges(fakeSimpleChanges);
   });
 });
