b4d22830836a314769855048d0f465625f6345cc
DannielDev <dannielnavas@gmail.com>
2022-01-26 14:53:49 -0500

    ✨ adjusment posts

--- src/app/shared/pipes/repo-list.pipe.ts
+++ src/app/shared/pipes/repo-list.pipe.ts
@@ -8,17 +8,21 @@ export class RepoListPipe implements PipeTransform {
 
   transform(value: any[], ...args: unknown[]): unknown {
     if (value) {
-      let random = 0;
       let repos = [];
-      let history = [];
-      for(let i = 0; i < args[0]; i++) {
-        random = Math.floor(Math.random() * value.length);
-        if (history.filter(item => item === random).length === 0) {
-          repos.push(value[random]);
-          history.push(random);
-        } else {
-          i--;
+      if(Number(args[0]) !== 0) {
+        let random = 0;
+        let history = [];
+        for(let i = 0; i < args[0]; i++) {
+          random = Math.floor(Math.random() * value.length);
+          if (history.filter(item => item === random).length === 0) {
+            repos.push(value[random]);
+            history.push(random);
+          } else {
+            i--;
+          }
         }
+      } else {
+        repos = value;
       }
       return repos;
     }
